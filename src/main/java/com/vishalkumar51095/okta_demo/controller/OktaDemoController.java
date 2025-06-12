d6feb6ae69fb0ec16f9a09a94077677d5390724c
Vishal Kumar <vishalkumar51095@gmail.com>
2025-06-12 12:47:02 +0530

    First Commit for Okta

--- src/main/java/com/vishalkumar51095/okta_demo/controller/OktaDemoController.java
+++ src/main/java/com/vishalkumar51095/okta_demo/controller/OktaDemoController.java
@@ -12,7 +12,6 @@ public class OktaDemoController {
 
     @GetMapping("/secured")
     public ResponseEntity<String> getStringByPassingToken(){
-
         return new ResponseEntity<>("Congratulations !! our access token is valid Go ahead!!", HttpStatus.OK);
     }
 }
